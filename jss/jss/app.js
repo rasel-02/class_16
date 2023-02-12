7d02cc292b09335b9f44cedc0a0ae29fa26da49f
Rasel Ahmed <117720471+rasel-02@users.noreply.github.com>
2023-02-13 00:09:09 +0600

    Add files via upload

---
+++ jss/jss/app.js
@@ -0,0 +1,9 @@
+ $('#sarch').hide();
+
+$('#sarch_Btn').on('click',function() {     $('#sarch').slideToggle();
+})
+
+$('#sidebar').hide();
+
+$('#side_btn').on('click',function() {
+ $('#sidebar').slideToggle(500); })
